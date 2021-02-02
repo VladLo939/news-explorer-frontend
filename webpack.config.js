bcc14225226eb5dd4a663ef462f8fa4d02fbbf5d
VladLo939 <vlo194@yandex.ru>
2021-02-02 14:09:53 +0300

    установил и настроил file loader, image-webpack loder, optimize css

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: { main: './src/script.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js'
@@ -20,17 +21,45 @@ module.exports = {
       {
         test: /\.css$/,
         use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      }
+      },
+      {
+        test: /\.(woff|woff2|ttf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "./fonts/[name].[ext]",
+          },
+        },
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "./images/[name].[ext]",
+            esModule: false,
+          },
+        },
+      },
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ //
-      filename: 'style.[contenthash].css',
+      filename: '[name].[contenthash].css',
     }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
       filename: 'index.html'
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default"],
+      },
+      canPrint: true,
     }),
     new WebpackMd5Hash()
   ]
